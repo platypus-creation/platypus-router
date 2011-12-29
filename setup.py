246dbfe7c1fd98819c175ae9d5f4bd4e7c6b8952
Vincent Jauneau <vincent.jauneau@platypus-creation.com>
2011-12-29 15:59:02 +0100

    Added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('README') as file:
+    long_description = file.read()
+
 setup(
     name = 'platypus-router',
     version = '0.1.0',
     description = 'Django like WSGI router',
+    long_description=long_description,
     author = 'Vincent Jauneau',
     author_email = 'vincent.jauneau@platypus-creation.com',
     url = 'https://github.com/platypus-creation/platypus-router/',
